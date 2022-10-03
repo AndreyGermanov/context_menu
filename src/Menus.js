7988f24eb2403c429927e3dedffb52b8cb106772
Andrey Germanov <andrey@germanov.dev>
2022-10-03 14:41:07 +0300

    Issue #2

--- src/Menus.js
+++ src/Menus.js
@@ -1,5 +1,10 @@
+import Menu from "./Menu.js";
+
 function Menus() {
 
+    this.create = (items,container,eventName) => {
+        return new Menu(items,container,eventName).init();
+    }
 }
 
 export default new Menus();
